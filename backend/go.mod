9cd9fecb841e866f20b7a2d3209bb7d9c4d0b5b5
guanw <guanw0826@gmail.com>
2020-02-05 19:43:05 -0500

    make kube works

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,4 @@ module github.com/kubernetes-playgrounds/kubernetes-backend-redis/backend
 
 go 1.13
 
-require github.com/go-redis/redis/v7 v7.0.0-beta.6 // indirect
+require github.com/go-redis/redis/v7 v7.0.0-beta.6
--- backend/main.go
+++ backend/main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/go-redis/redis/v7"
 )
 
 func main() {
+	env := os.Getenv("ENV")
+	var endpoint string
+	switch env {
+	case "PRODUCTION":
+		endpoint = "redis-test"
+	default:
+		endpoint = "0.0.0.0"
+	}
 	client := redis.NewClient(&redis.Options{
-		Addr:     "0.0.0.0:6379",
+		Addr:     endpoint + ":6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
